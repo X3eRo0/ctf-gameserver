b8f74c2fcab91139553f94d6e264e3e2948b51d7
x3ero0 <psinghania929@gmail.com>
2025-07-07 01:09:50 -0700

    Fixed force_text, force_str

--- src/ctf_gameserver/web/templatetags/templatetags/form_as_div.py
+++ src/ctf_gameserver/web/templatetags/templatetags/form_as_div.py
@@ -1,12 +1,18 @@
 from django import template
-from django.forms import CheckboxInput, CheckboxSelectMultiple, RadioSelect, FileInput, MultiWidget
+from django.forms import (
+    CheckboxInput,
+    CheckboxSelectMultiple,
+    RadioSelect,
+    FileInput,
+    MultiWidget,
+)
 from django.forms.utils import ErrorList
-from django.utils.encoding import force_text
+from django.utils.encoding import force_str
 from django.utils.html import format_html, format_html_join, conditional_escape
 from django.utils.translation import ugettext as _
 from django.utils.safestring import mark_safe
 
-register = template.Library()    # pylint: disable=invalid-name
+register = template.Library()  # pylint: disable=invalid-name
 
 
 class _ListGroupErrorList(ErrorList):
@@ -15,8 +21,11 @@ class _ListGroupErrorList(ErrorList):
     """
 
     def as_ul(self):
-        elements = format_html_join('\n', '<li class="list-group-item list-group-item-danger">{}</li>',
-                                    ((e,) for e in self))
+        elements = format_html_join(
+            "\n",
+            '<li class="list-group-item list-group-item-danger">{}</li>',
+            ((e,) for e in self),
+        )
         return format_html('<ul class="list-group">\n{}\n</ul>', elements)
 
 
@@ -40,30 +49,30 @@ def _get_css_classes(field):
     wrap_in_label = False
 
     if field.errors:
-        div_classes.append('has-error')
+        div_classes.append("has-error")
 
     widget = field.field.widget
 
     if isinstance(widget, CheckboxInput):
-        div_classes.append('checkbox')
+        div_classes.append("checkbox")
         wrap_in_label = True
     elif isinstance(widget, CheckboxSelectMultiple):
-        div_classes.append('form-group')
-        field_classes.append('checkbox')
+        div_classes.append("form-group")
+        field_classes.append("checkbox")
         iterate_subfields = True
     elif isinstance(widget, RadioSelect):
-        div_classes.append('form-group')
-        field_classes.append('radio')
+        div_classes.append("form-group")
+        field_classes.append("radio")
         iterate_subfields = True
     elif isinstance(widget, FileInput):
         pass
     elif isinstance(widget, MultiWidget):
-        div_classes.append('form-group')
-        div_classes.append('form-inline')
-        field_classes.append('form-control')
+        div_classes.append("form-group")
+        div_classes.append("form-inline")
+        field_classes.append("form-control")
     else:
-        div_classes.append('form-group')
-        field_classes.append('form-control')
+        div_classes.append("form-group")
+        field_classes.append("form-control")
 
     return (div_classes, field_classes, iterate_subfields, wrap_in_label)
 
@@ -79,28 +88,44 @@ def as_div(form):
     """
 
     output = []
-    top_errors = _ListGroupErrorList([conditional_escape(e) for e in form.non_field_errors()])
+    top_errors = _ListGroupErrorList(
+        [conditional_escape(e) for e in form.non_field_errors()]
+    )
 
     for field in form.hidden_fields():
         # Display errors for hidden fields in the global error list
         for error in field.errors:
-            top_errors.append(format_html(_('Hidden field {name}: {error}'), name=field.name, error=error))
+            top_errors.append(
+                format_html(
+                    _("Hidden field {name}: {error}"), name=field.name, error=error
+                )
+            )
         output.append(str(field))
 
     for field in form.visible_fields():
-        div_classes, field_classes, iterate_subfields, wrap_in_label = _get_css_classes(field)
+        div_classes, field_classes, iterate_subfields, wrap_in_label = _get_css_classes(
+            field
+        )
 
-        div_class_string = format_html_join(' ', '{}', [(mark_safe(c),) for c in div_classes])
-        div_class_string += mark_safe(' ') + conditional_escape(force_text(field.css_classes()))
-        field_class_string = format_html_join(' ', '{}', [(mark_safe(c),) for c in field_classes])
+        div_class_string = format_html_join(
+            " ", "{}", [(mark_safe(c),) for c in div_classes]
+        )
+        div_class_string += mark_safe(" ") + conditional_escape(
+            force_str(field.css_classes())
+        )
+        field_class_string = format_html_join(
+            " ", "{}", [(mark_safe(c),) for c in field_classes]
+        )
 
         output.append(format_html('<div class="{}">', div_class_string))
 
         if not wrap_in_label:
-            output.append(force_text(field.label_tag()))
+            output.append(force_str(field.label_tag()))
 
         if field.errors:
-            output.append(str(_ListGroupErrorList([conditional_escape(e) for e in field.errors])))
+            output.append(
+                str(_ListGroupErrorList([conditional_escape(e) for e in field.errors]))
+            )
 
         # The default rendering for RadioSelect and CheckboxSelectMultiple wraps the options in a list, but
         # Bootstrap requires `<div>`s
@@ -108,38 +133,51 @@ def as_div(form):
             rendered_parts = []
 
             for subfield in field:
-                rendered_parts.append(format_html('<div class="{}">', field_class_string))
-                rendered_parts.append(force_text(subfield))
-                rendered_parts.append(mark_safe('</div>'))
+                rendered_parts.append(
+                    format_html('<div class="{}">', field_class_string)
+                )
+                rendered_parts.append(force_str(subfield))
+                rendered_parts.append(mark_safe("</div>"))
 
-            rendered_field = format_html_join('\n', '{}', ((p,) for p in rendered_parts))
+            rendered_field = format_html_join(
+                "\n", "{}", ((p,) for p in rendered_parts)
+            )
         else:
-            rendered_field = force_text(field.as_widget(attrs={'class': field_class_string}))
+            rendered_field = force_str(
+                field.as_widget(attrs={"class": field_class_string})
+            )
 
         # Bootstrap requires to actial `<input>` element to be inside the associated `<label>` for
         # checkboxes
         if wrap_in_label:
-            label_content = rendered_field + mark_safe(' ') + conditional_escape(force_text(field.label))
-            output.append(force_text(field.label_tag(label_content, label_suffix='')))
+            label_content = (
+                rendered_field
+                + mark_safe(" ")
+                + conditional_escape(force_str(field.label))
+            )
+            output.append(force_str(field.label_tag(label_content, label_suffix="")))
         else:
             output.append(rendered_field)
 
         # 'show_hidden_initial' on fields means that they should additionally be rendered hidden for
         # comparisons between the initial and the current value
         if field.field.show_hidden_initial:
-            output.append(force_text(field.as_hidden(only_initial=True)))
+            output.append(force_str(field.as_hidden(only_initial=True)))
 
-        help_text = conditional_escape(force_text(field.help_text))
+        help_text = conditional_escape(force_str(field.help_text))
 
         if not field.field.required:
-            help_text = mark_safe('<span class="label label-default">Optional</span> ') + help_text
+            help_text = (
+                mark_safe('<span class="label label-default">Optional</span> ')
+                + help_text
+            )
 
         if help_text:
             output.append(format_html('<span class="help-block">{}</span>', help_text))
 
-        output.append(mark_safe('</div>'))
+        output.append(mark_safe("</div>"))
 
     if top_errors:
-        output.insert(0, force_text(top_errors))
+        output.insert(0, force_str(top_errors))
 
-    return format_html_join('\n', '{}', ((o,) for o in output))
+    return format_html_join("\n", "{}", ((o,) for o in output))
