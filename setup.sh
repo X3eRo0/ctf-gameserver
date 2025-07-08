aeed939e7021b4e1ae7b3ffc0a34e92a4a187eb7
x3ero0 <psinghania929@gmail.com>
2025-07-07 23:20:02 -0700

    Fix.

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ apt update
 apt install -y devscripts dpkg-dev equivs neovim postgresql python3-psycopg2 nginx uwsgi uwsgi-plugin-python3 postfix
 mk-build-deps --install debian/control
 dpkg-buildpackage --unsigned-changes --unsigned-buildinfo
-cd /root/ && python3 -m http.server 8000 --bind 0.0.0.0 &
+cd /root/ && python3 -m http.server 8000 --bind 0.0.0.0
